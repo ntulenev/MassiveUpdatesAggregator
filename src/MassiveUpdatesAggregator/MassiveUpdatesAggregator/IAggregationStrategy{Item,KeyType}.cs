7c66c8b71e8ff41d7dbdc67320d1b40e19bc1952
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-08 14:17:36 +0300

    Add IAggregationStrategy

---
+++ src/MassiveUpdatesAggregator/MassiveUpdatesAggregator/IAggregationStrategy{Item,KeyType}.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MassiveUpdatesAggregator
+{
+    public interface IAggregationStrategy<Item, KeyType> where Item : IAggregatorItem<KeyType>
+    {
+        public Item Merge(IEnumerable<Item> items);
+    }
+}
