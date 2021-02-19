b8098feb3f4917364d55c1149919f587b36ad12d
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-19 22:14:14 +0300

    Fix namespaces formatting

--- src/MassiveUpdatesAggregator/MassiveUpdatesAggregator.Tests/Aggregator{Item,KeyType}.Tests.cs
+++ src/MassiveUpdatesAggregator/MassiveUpdatesAggregator.Tests/Aggregator{Item,KeyType}.Tests.cs
@@ -1,11 +1,12 @@
-﻿using FluentAssertions;
-using Moq;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using FluentAssertions;
+using Moq;
+
 using Xunit;
 
 namespace MassiveUpdatesAggregator.Tests
