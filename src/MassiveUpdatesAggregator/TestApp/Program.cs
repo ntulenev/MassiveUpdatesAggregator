b5e45097f225bc151cadc98d48344c3f87d359f8
Nikita Tulenev <n1tulenev@gmail.com>
2022-12-19 20:22:41 +0100

    Fix namespace

--- src/MassiveUpdatesAggregator/TestApp/Program.cs
+++ src/MassiveUpdatesAggregator/TestApp/Program.cs
@@ -1,80 +1,79 @@
 ﻿using MassiveUpdatesAggregator;
 
-namespace TestApp
+namespace TestApp;
+
+public record TestData(int Key, string Value) : IAggregatorItem<int>;
+
+/// <summary>
+/// This class implements aggregation strategy for bunch of data
+/// </summary>
+public class SumStrategy : IAggregationStrategy<TestData, int>
 {
-    public record TestData(int Key, string Value) : IAggregatorItem<int>;
-
-    /// <summary>
-    /// This class implements aggregation strategy for bunch of data
-    /// </summary>
-    public class SumStrategy : IAggregationStrategy<TestData, int>
+    public TestData Merge(IEnumerable<TestData> items)
     {
-        public TestData Merge(IEnumerable<TestData> items)
-        {
-            ArgumentNullException.ThrowIfNull(items);
+        ArgumentNullException.ThrowIfNull(items);
 
-            // Simple strategy - just create new item with key and contact of all values
-            var key = items.First().Key;
-            return new TestData(Key: key, Value: string.Join("-", items.Select(x => x.Value)));
-        }
+        // Simple strategy - just create new item with key and contact of all values
+        var key = items.First().Key;
+        return new TestData(Key: key, Value: string.Join("-", items.Select(x => x.Value)));
     }
+}
 
-    class Program
+class Program
+{
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        var testData = "The quick brown fox jumps over the lazy dog".Split(" ");
+
+        using var cts = new CancellationTokenSource();
+
+        var rnd = new Random();
+
+        // Creation of the aggregator class
+        var aggregator = new Aggregator<TestData, int>(initialSize: 100, millisecondsDelay: 1000, new SumStrategy(), cts.Token);
+
+        bool isWork = true;
+
+        int i = 0;
+
+        _ = Task.Run(async () =>
         {
-            var testData = "The quick brown fox jumps over the lazy dog".Split(" ");
-
-            using var cts = new CancellationTokenSource();
-
-            var rnd = new Random();
-
-            // Creation of the aggregator class
-            var aggregator = new Aggregator<TestData, int>(initialSize: 100, millisecondsDelay: 1000, new SumStrategy(), cts.Token);
-
-            bool isWork = true;
-
-            int i = 0;
-
-            _ = Task.Run(async () =>
+            while (isWork)
             {
-                while (isWork)
+                await Task.Delay(100 * rnd.Next(1, 7));
+
+                // Send data for key = 1  
+                await aggregator.SendAsync(new TestData(Key: 1, Value: testData[i++]));
+
+                if (i == testData.Length)
                 {
-                    await Task.Delay(100 * rnd.Next(1, 7));
-
-                    // Send data for key = 1  
-                    await aggregator.SendAsync(new TestData(Key: 1, Value: testData[i++]));
-
-                    if (i == testData.Length)
-                    {
-                        i = 0;
-                    }
+                    i = 0;
                 }
-            });
-
-            _ = Task.Run(async () =>
-            {
-                // Iterate aggregated items
-                await foreach (var item in aggregator.WithCancellation(cts.Token))
-                {
-                    Console.WriteLine($"{item.Key} {item.Value}");
-                }
-            });
-
-            Console.ReadKey();
-            cts.Cancel();
-            isWork = false;
-            try
-            {
-                // Stop aggregator
-                await aggregator.StopAsync();
             }
-            catch (TaskCanceledException)
+        });
+
+        _ = Task.Run(async () =>
+        {
+            // Iterate aggregated items
+            await foreach (var item in aggregator.WithCancellation(cts.Token))
             {
-
+                Console.WriteLine($"{item.Key} {item.Value}");
             }
+        });
 
-            Console.WriteLine("All done.");
+        Console.ReadKey();
+        cts.Cancel();
+        isWork = false;
+        try
+        {
+            // Stop aggregator
+            await aggregator.StopAsync();
         }
+        catch (TaskCanceledException)
+        {
+
+        }
+
+        Console.WriteLine("All done.");
     }
 }
