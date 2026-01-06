871dcc0979053545e0214e8e25c2cacec9f8817e
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-06 09:17:59 +0100

    Suppresses CA1515 warning for test item
    Suppresses CA1515 warning in the test item class.
    
    The public type is needed for Moq to create mocks.

--- src/MassiveUpdatesAggregator/MassiveUpdatesAggregator.Tests/TestItem.cs
+++ src/MassiveUpdatesAggregator/MassiveUpdatesAggregator.Tests/TestItem.cs
@@ -1,9 +1,11 @@
-﻿namespace MassiveUpdatesAggregator.Tests;
+namespace MassiveUpdatesAggregator.Tests;
 
 /// <summary>
 /// Test object to for aggregation tests.
 /// </summary>
+#pragma warning disable CA1515 // Consider making public types internal. Need to be public for Moq
 public sealed record TestItem : IAggregatorItem<object>
+#pragma warning restore CA1515 // Consider making public types internal
 {
     /// <summary>
     /// Aggregation key.
