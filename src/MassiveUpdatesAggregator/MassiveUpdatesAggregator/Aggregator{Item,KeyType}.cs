d6c3f481c801e4a1ba522706f9b7755b998fe7de
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-06 09:17:52 +0100

    Renames generics, improves readability
    Renames generic types to follow .NET naming conventions,
    improving code clarity and readability.
    
    Refactors code for better maintainability.

--- src/MassiveUpdatesAggregator/MassiveUpdatesAggregator/Aggregator{Item,KeyType}.cs
+++ src/MassiveUpdatesAggregator/MassiveUpdatesAggregator/Aggregator{Item,KeyType}.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Channels;
+using System.Threading.Channels;
 
 using Nito.AsyncEx;
 
@@ -7,26 +7,26 @@ namespace MassiveUpdatesAggregator;
 /// <summary>
 /// Class that aggregates items per Key.
 /// </summary>
-/// <typeparam name="Item">Type of the item.</typeparam>
-/// <typeparam name="KeyType">Type of the item key.</typeparam>
-public sealed class Aggregator<Item, KeyType> :
-            IAsyncEnumerable<Item>
-            where Item : IAggregatorItem<KeyType>
-            where KeyType : notnull
+/// <typeparam name="TItem">Type of the item.</typeparam>
+/// <typeparam name="TKeyType">Type of the item key.</typeparam>
+public sealed class Aggregator<TItem, TKeyType> :
+            IAsyncEnumerable<TItem>
+            where TItem : IAggregatorItem<TKeyType>
+            where TKeyType : notnull
 {
     /// <summary>
     /// Creates aggregator
     /// </summary>
     /// <param name="initialSize">Approximate size of the number of keys.</param>
     /// <param name="delay">Delay when gets aggregates data from items.</param>
-    /// <param name="strategy">Aggregation strategy <see cref="IAggregationStrategy"/>.</param>
+    /// <param name="strategy">Aggregation strategy <see cref="IAggregationStrategy{Item, KeyType}"/>.</param>
     /// <param name="ct">Cancellation token for async operations.</param>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="strategy"/> is null.</exception>
     /// <exception cref="ArgumentException">Thrown when <paramref name="initialSize"/> 
-    /// or <paramref name="millisecondsDelay"/> is incorrect.</exception>
+    /// or <paramref name="delay"/> is incorrect.</exception>
     public Aggregator(int initialSize,
                       TimeSpan delay,
-                      IAggregationStrategy<Item, KeyType> strategy,
+                      IAggregationStrategy<TItem, TKeyType> strategy,
                       CancellationToken ct = default)
     {
         if (initialSize <= 0)
@@ -41,11 +41,11 @@ public sealed class Aggregator<Item, KeyType> :
 
         ArgumentNullException.ThrowIfNull(strategy);
 
-        _items = new Dictionary<KeyType, LinkedList<Item>>(initialSize);
-        _scheduledWork = new Dictionary<KeyType, Task>(initialSize);
+        _items = new Dictionary<TKeyType, LinkedList<TItem>>(initialSize);
+        _scheduledWork = new Dictionary<TKeyType, Task>(initialSize);
         _ct = ct;
         _delay = delay;
-        _channel = Channel.CreateUnbounded<IEnumerable<Item>>();
+        _channel = Channel.CreateUnbounded<IEnumerable<TItem>>();
         _strategy = strategy;
     }
 
@@ -55,11 +55,11 @@ public sealed class Aggregator<Item, KeyType> :
     public bool IsRunning => _isRunning;
 
     /// <summary>
-    /// Add <typeparamref name="Item"/> in aggregator.
+    /// Add <typeparamref name="TItem"/> in aggregator.
     /// </summary>
     /// <exception cref="ArgumentNullException">Thrown when 
-    /// <typeparamref name="Item"/> is null.</exception>
-    public async Task SendAsync(Item item)
+    /// <typeparamref name="TItem"/> is null.</exception>
+    public async Task SendAsync(TItem item)
     {
         ArgumentNullException.ThrowIfNull(item);
 
@@ -67,19 +67,21 @@ public sealed class Aggregator<Item, KeyType> :
 
         using (await _guard.LockAsync(_ct).ConfigureAwait(false))
         {
-            if (_isRunning is false)
-                return;
-
-            if (!_items.TryGetValue(key, out LinkedList<Item>? list))
+            if (!_isRunning)
             {
-                list = new LinkedList<Item>();
+                return;
+            }
+
+            if (!_items.TryGetValue(key, out var list))
+            {
+                list = new LinkedList<TItem>();
                 _items.Add(key, list);
-                list.AddLast(item);
-                _scheduledWork.Add(key, CreateAggregationTask(key));
+                _ = list.AddLast(item);
+                _scheduledWork.Add(key, CreateAggregationTaskAsync(key));
             }
             else
             {
-                list.AddLast(item);
+                _ = list.AddLast(item);
             }
         }
     }
@@ -89,11 +91,11 @@ public sealed class Aggregator<Item, KeyType> :
     /// </summary>
     /// <param name="cancellationToken"></param>
     /// <returns>Messages async iterator.</returns>
-    public async IAsyncEnumerator<Item> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    public async IAsyncEnumerator<TItem> GetAsyncEnumerator(CancellationToken cancellationToken = default)
     {
         while (await _channel.Reader.WaitToReadAsync(cancellationToken).ConfigureAwait(false))
         {
-            while (_channel.Reader.TryRead(out IEnumerable<Item>? items))
+            while (_channel.Reader.TryRead(out var items))
             {
                 yield return _strategy.Merge(items);
             }
@@ -109,24 +111,24 @@ public sealed class Aggregator<Item, KeyType> :
 
         using (await _guard.LockAsync(_ct).ConfigureAwait(false))
         {
-            if (_isRunning is false)
+            if (!_isRunning)
             {
                 throw new InvalidOperationException("Aggregator is already in stopping state.");
             }
 
             _isRunning = false;
 
-            tasks = _scheduledWork.Select(x => x.Value).ToList();
+            tasks = [.. _scheduledWork.Select(x => x.Value)];
         }
 
-        await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks).ConfigureAwait(false);
     }
 
-    private async Task CreateAggregationTask(KeyType key)
+    private async Task CreateAggregationTaskAsync(TKeyType key)
     {
         await Task.Delay(_delay, _ct).ConfigureAwait(false);
 
-        LinkedList<Item>? list;
+        LinkedList<TItem>? list;
 
         using (await _guard.LockAsync(_ct).ConfigureAwait(false))
         {
@@ -136,20 +138,20 @@ public sealed class Aggregator<Item, KeyType> :
                         $"List with data for key {key} not found");
             }
 
-            _items.Remove(key);
-            _scheduledWork.Remove(key);
+            _ = _items.Remove(key);
+            _ = _scheduledWork.Remove(key);
 
         }
 
         await _channel.Writer.WriteAsync(list, _ct).ConfigureAwait(false);
     }
 
-    private readonly Dictionary<KeyType, Task> _scheduledWork;
-    private readonly Dictionary<KeyType, LinkedList<Item>> _items;
+    private readonly Dictionary<TKeyType, Task> _scheduledWork;
+    private readonly Dictionary<TKeyType, LinkedList<TItem>> _items;
     private readonly AsyncLock _guard = new();
     private readonly CancellationToken _ct;
-    private readonly Channel<IEnumerable<Item>> _channel;
-    private readonly IAggregationStrategy<Item, KeyType> _strategy;
+    private readonly Channel<IEnumerable<TItem>> _channel;
+    private readonly IAggregationStrategy<TItem, TKeyType> _strategy;
     private volatile bool _isRunning = true;
     private readonly TimeSpan _delay;
 }
